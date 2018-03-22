53ecc3dae77d675f7f2f14f702c18afb010af437
Julián García <julian2996@gmail.com>
2018-03-22 03:52:05 -0600

    Update tweepstr.py

--- tweepstr.py
+++ tweepstr.py
@@ -125,7 +125,7 @@ async def main():
     init = -1
     num = 0
     while True:
-        print(num, end='')
+        print(num, end=' ')
         if num > 500:
             break
         if len(feed) > 0:
