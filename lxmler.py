68260307471ded4bee6ac830e48f77383f311482
Julián García <julian2996@gmail.com>
2018-03-21 21:00:01 -0500

    Add files via upload

---
+++ lxmler.py
@@ -0,0 +1,163 @@
+import traceback
+from difflib import SequenceMatcher
+
+from lxml import html
+
+import requests
+from openpyxl import load_workbook
+from selenium.common.exceptions import TimeoutException
+
+import sqlite3
+
+from company_list.tweepstr import get_tweets
+
+conn = sqlite3.connect('companies2.db')
+cur = conn.cursor()
+cur.execute('CREATE TABLE companies (input_name text, title text, type text, industry text, founded text, founder text, headquarters text, key_people text, products text, revenue text, operating_income text, net_income text, owners text, website text, traded_as text, subsidiaries text, area_served text, parent text, divisions text, link text, tweets text)')
+query = 'INSERT INTO companies VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+
+print(requests.__version__)
+
+wb = load_workbook('client_list.xlsx')
+ws = wb.active
+def get_info(tree, input_name, url):
+    info = tree.xpath('//*[@id="mw-content-text"]/div/table')[0]
+    title = tree.xpath('//*[@id="firstHeading"]')[0].text_content()
+    type = ''
+    industry = ''
+    founded = ''
+    founder = ''
+    headquarters = ''
+    key_people = ''
+    products = ''
+    revenue = ''
+    operating_income = ''
+    net_income = ''
+    owners = ''
+    website = ''
+
+    traded_as = ''
+    subsidiaries = ''
+
+    area_served = ''
+    parent = ''
+    divisions = ''
+    for row in info:
+        try:
+            thead = row.xpath('.//th')[0]
+            field = row.xpath('.//td')[0]
+            t = thead.text_content().lower()
+            f = field.text_content().strip()
+            if 'traded' in info.text_content().lower():
+                traded_as = field.text_content().strip()
+            if 'type' in thead.text_content().lower():
+                type = field.text_content().strip()
+            if 'industry' in thead.text_content().lower():
+                industry = field.text_content().strip()
+            if 'founded' in thead.text_content().lower():
+                founded = field.text_content().strip()
+            if 'founder' in thead.text_content().lower():
+                founder = field.text_content().strip()
+            if 'headquarters' in thead.text_content().lower():
+                headquarters = field.text_content().strip()
+            if 'key people' in thead.text_content().lower():
+                key_people = field.text_content().strip()
+            if 'products' in thead.text_content().lower():
+                products = field.text_content().strip()
+            if 'revenue' in thead.text_content().lower():
+                revenue = field.text_content().strip()
+            if 'operating income' in thead.text_content().lower():
+                operating_income = field.text_content().strip()
+            if 'net income' in thead.text_content().lower():
+                net_income = field.text_content().strip()
+            if 'owner' in thead.text_content().lower():
+                owners = field.text_content().strip()
+            if 'website' in thead.text_content().lower():
+                website = field.text_content().strip()
+            if 'traded as' in thead.text_content().lower():
+                traded_as = field.text_content().strip()
+            if 'subsidiaries' in thead.text_content().lower():
+                subsidiaries = field.text_content().strip()
+
+            if 'area' in thead.text_content().lower():
+                area_served = field.text_content().strip()
+            if 'parent' in thead.text_content().lower():
+                parent = field.text_content().strip()
+            if 'division' in thead.text_content().lower():
+                divisions = field.text_content().strip()
+        except:
+            # traceback.print_exc()
+            continue
+    print(type,
+          industry,
+          founded,
+          founder,
+          headquarters,
+          key_people,
+          products,
+          revenue,
+          operating_income,
+          net_income,
+          owners,
+          website,
+          traded_as,
+          subsidiaries,
+          area_served,
+          parent,
+          divisions)
+    tweets = ''.join(get_tweets(input_name))
+    cur.execute(query, (input_name, title, type, industry, founded, founder, headquarters, key_people, products, revenue, operating_income, net_income, owners, website, traded_as, subsidiaries, area_served, parent, divisions, og_url, tweets))
+    conn.commit()
+    # print('\nd\n')
+
+def _gen_tree(url):
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    page.close()
+    return tree, page.url
+
+base_url = 'https://en.wikipedia.org'
+for i in range(2, 7501):
+    try:
+        name = ws['C' + str(i)].value
+        url = 'https://en.wikipedia.org/w/index.php?search=' + name.replace(' ', '+').replace('&', '%26')
+        tree, og_url = _gen_tree(url)
+        # wait_for_xpath(driver, '//ul[@class="mw-search-results"]', time=3)
+        results = tree.xpath('//ul[@class="mw-search-results"]/li')
+        if len(results) < 1:
+            try:
+                get_info(tree, name, og_url)
+            except:
+                traceback.print_exc()
+                pass
+            continue
+        matches = []
+        for result in results:
+            res_text = result.text_content()
+            s = SequenceMatcher(None, name, result.text_content().split('   ')[0])
+            # if s.ratio() > .65:
+            #     print(name, '||||||||', result.text_content().split('   ')[0], s.ratio())
+            if name in result.text_content().split('   ')[0] or result.text_content().split('   ')[0] in name or s.ratio() > .70:
+                print('MATCH!', name, '||||||||', result.text_content().split('   ')[0], s.ratio())
+                matches.append(result.xpath('./div[1]/a')[0].get('href'))
+            else:
+                if name in res_text:
+                    print(name, result.text_content())
+                    print()
+            # print(result.text)
+        for match in matches:
+            try:
+                # pass
+                tree, og_url = _gen_tree(base_url + match)
+                get_info(tree, name, og_url)
+            except:
+                traceback.print_exc()
+                pass
+        if len(matches) == 0:
+            tweets = ''.join(get_tweets(name))
+            cur.execute(query, (name, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', tweets))
+            conn.commit()
+    except TimeoutException:
+        traceback.print_exc()
+        get_info(tree)
+print('l')
---
+++ tweepstr.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python3
+from bs4 import BeautifulSoup
+from time import gmtime, strftime
+import aiohttp
+import asyncio
+import async_timeout
+import datetime
+import json
+import re
+import sys
+import time
+
+async def getUrl(init):
+    if init == -1:
+        url = "https://twitter.com/search?f=tweets&vertical=default&lang=en&q="
+    else:
+        url = "https://twitter.com/i/search/timeline?f=tweets&vertical=default"
+        url+= "&lang=en&include_available_features=1&include_entities=1&reset_"
+        url+= "error_state=false&src=typd&max_position={}&q=".format(init)
+
+    # url+= "from%3A{0}".format('juliangabu')
+    url += "%20{0}".format(company)
+    url += "%20since%3A{0}".format(since)
+    return url
+
+async def fetch(session, url):
+    with async_timeout.timeout(30):
+        async with session.get(url) as response:
+            return await response.text()
+
+async def initial(response):
+    soup = BeautifulSoup(response, "html.parser")
+    feed = soup.find_all("li", "js-stream-item")
+    init = "TWEET-{}-{}".format(feed[-1]["data-item-id"], feed[0]["data-item-id"])
+
+    return feed, init
+
+async def cont(response):
+    json_response = json.loads(response)
+    html = json_response["items_html"]
+    soup = BeautifulSoup(html, "html.parser")
+    feed = soup.find_all("li", "js-stream-item")
+    split = json_response["min_position"].split("-")
+    split[1] = feed[-1]["data-item-id"]
+    init = "-".join(split)
+
+    return feed, init
+
+async def getFeed(init):
+    async with aiohttp.ClientSession() as session:
+        response = await fetch(session, await getUrl(init))
+    feed = []
+    try:
+        if init == -1:
+            feed, init = await initial(response)
+        else:
+            feed, init = await cont(response)
+    except:
+        pass
+    return feed, init
+
+async def outTweet(tweet):
+    tweetid = tweet["data-item-id"]
+    # Formatting the date & time stamps just how I like it.
+    datestamp = tweet.find("a", "tweet-timestamp")["title"].rpartition(" - ")[-1]
+    d = datetime.datetime.strptime(datestamp, "%d %b %Y")
+    date = d.strftime("%Y-%m-%d")
+    timestamp = str(datetime.timedelta(seconds=int(tweet.find("span", "_timestamp")["data-time"]))).rpartition(", ")[-1]
+    t = datetime.datetime.strptime(timestamp, "%H:%M:%S")
+    time = t.strftime("%H:%M:%S")
+    # The @ in the username annoys me.
+    username = tweet.find("span", "username").text.replace("@", "")
+    timezone = strftime("%Z", gmtime())
+    # The context of the Tweet compressed into a single line.
+    text = tweet.find("p", "tweet-text").text.replace("\n", "").replace("http", " http").replace("pic.twitter", " pic.twitter")
+    # Regex for gathering hashtags
+    hashtags = ",".join(re.findall(r'(?i)\#\w+', text, flags=re.UNICODE))
+    replies = tweet.find("span", "ProfileTweet-action--reply u-hiddenVisually").find("span")["data-tweet-stat-count"]
+    retweets = tweet.find("span", "ProfileTweet-action--retweet u-hiddenVisually").find("span")["data-tweet-stat-count"]
+    likes = tweet.find("span", "ProfileTweet-action--favorite u-hiddenVisually").find("span")["data-tweet-stat-count"]
+    try:
+        mentions = tweet.find("div", "js-original-tweet")["data-mentions"].split(" ")
+        for i in range(len(mentions)):
+            mention = "@{}".format(mentions[i])
+            if mention not in text:
+                text = "{} {}".format(mention, text)
+    except:
+        pass
+
+    # output = ""
+    # output = "{} {} {} {} <{}> {}".format(tweetid, date, time, timezone, username, text)
+    output = "{} {} <@{}> {}".format(date, time, username, text)
+    global tweets_output
+    tweets_output.append("{} {} <@{}> {}\n".format(date, time, username, text))
+    # if arg.hashtags:
+    #     output+= " {}".format(hashtags)
+    # if arg.stats:
+    #     output+= " | {} replies {} retweets {} likes".format(replies, retweets, likes)
+
+    return output
+
+async def getTweets(init):
+    tweets, init = await getFeed(init)
+    count = 0
+    for tweet in tweets:
+        '''
+        Certain Tweets get taken down for copyright but are still
+        visible in the search. We want to avoid those.
+        '''
+        copyright = tweet.find("div","StreamItemContent--withheld")
+        if copyright is None:
+            count +=1
+            print(await outTweet(tweet))
+
+    return tweets, init, count
+
+# async def getUsername():
+#     async with aiohttp.ClientSession() as session:
+#         r = await fetch(session, "https://twitter.com/intent/user?user_id={0.userid}".format(arg))
+#     soup = BeautifulSoup(r, "html.parser")
+#     return soup.find("a", "fn url alternate-context")["href"].replace("/", "")
+
+async def main():
+    feed = [-1]
+    init = -1
+    num = 0
+    while True:
+        if len(feed) > 0:
+            feed, init, count = await getTweets(init)
+            num += count
+        else:
+            break
+
+def Error(error, message):
+    # Error formatting
+    print("[-] {}: {}".format(error, message))
+    sys.exit(0)
+
+def get_tweets(input):
+    global since
+    global tweets_output
+    tweets_output = []
+    date = time.strftime("%Y-%m-%d").split('-')
+    date[1] = str(int(date[1]) - 1)
+    since = '-'.join(date)
+    global company
+    company = input
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    return tweets_output
